aa5f4c31342a3d39c77cf383285ce9199d454429
arthshah-97 <51439589+arthshah-97@users.noreply.github.com>
2019-07-14 19:38:21 +0530

    Add files via upload

---
+++ mqtt_wifi.ino
@@ -0,0 +1,46 @@
+#include <PubSubClient.h>
+
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <WiFiEspServer.h>
+#include <WiFiEspUdp.h>
+
+WiFiEspClient espclient;
+PubSubClient client(espclient);
+int status=WL_IDLE_STATUS;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+WiFi.init(&Serial);
+while(status!=WL_CONNECTED)
+{
+status=WiFi.begin("FD-28","soham111");
+delay(8000);
+}
+client.setServer("iot.eclipse.org",1883);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(!client.connected())
+{
+  pub();
+}
+client.loop();
+}
+
+void pub()
+{
+  while(!client.connected())
+  {
+    if(client.connect("arduinoClient"))
+    {
+      client.publish("mytopic/today","hi");
+      
+    }
+    else
+    {Serial.println("");delay(5000);}
+  }
+}
+
